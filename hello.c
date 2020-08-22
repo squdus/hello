e1b7c764ab9769061fbd54c22638a0cda54a22a5
squdus <54606844+squdus@users.noreply.github.com>
2020-08-22 13:55:16 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Sara Qudus
+ *
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+   printf("Sara Qudus!\n");
+    printf("Civil Engineering\n");
+
+  return 0;
+}
