4104d8fb1f696b95d7400e81632135ea93f6ed4e
LinneaPietsch <lpietsch2@huskers.unl.edu>
2020-08-26 00:54:21 +0000

    added name and major to partner repository

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
    printf("Sara Qudus!\n");
     printf("Civil Engineering\n");
-
+    printf("Linnea Pietsch\n");
+    printf("English\n");
+    
   return 0;
 }
